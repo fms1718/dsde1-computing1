f336b8e9fb11e33e1f4ce9d2627481ba5551e602
Freya Smith <freya.smith19@imperial.ac.uk>
2019-10-23 18:41:44 +0100

    week 3 testing

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -9,20 +9,29 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
-    print(a)
+    ans = a
+    new_ans = int(ans)
+    print(new_ans+1)
+    return new_ans + 1 
 
+user_input = input("input something")
+user_input = int(user_input)
+answer_2 = inc(user_input)
+a = user_input
+print(answer_2)
 
 # write a function that adds 1
 # to the input and returns the result
-def inc_return(a):
-    return # hint this is incomplete
 
+def inc_return(a):
+    b = inc(a)
+    return(b)
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    return a + b
 
 
 # write a function that takes two
@@ -30,14 +39,18 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    c = sum(a, b)
+    c = inc_return(c)
+    return c
+    
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    b = bool(a%2 == 0)
+    return(b)
 
 
 # create for loop that takes a string
@@ -47,7 +60,11 @@ def is_even(a):
 # e.g. string_repeat('ho', 3) returns
 # 'hohoho'
 def string_repeat(phrase, repeat):
+    x = phrase * repeat
+    return x
+
+
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+    
 
