6a8fc4c1dc35c2f9892b1b9c6fc77b3a74b546a6
Freya Smith <freya.smith19@imperial.ac.uk>
2019-10-29 10:23:59 +0000

    flowchart

--- Week-4/structures.py
+++ Week-4/structures.py
@@ -9,7 +9,12 @@ Simple functions performing operations on basic Python data structures.
 # write a function that returns a list containig the first and the last element
 # of "the_list". 
 def first_and_last(the_list):
-    return []
+    new_list = [the_list[0], the_list[-1]]
+    print(new_list)
+    return new_list
+
+    
+
 
 
 # write a function that returns part of "the_list" between indices given by the
@@ -20,6 +25,12 @@ def first_and_last(the_list):
 def part_reverse(the_list, beginning, end):
     return # hint this is incomplete
 
+def second_and_third(the_list):
+    changed_list = [[the_list[1]],[the_list[2]]]
+    print (changed_list)
+    changed_list.sort(reverse = True)
+    print(changed_list)
+    return changed_list
 
 # write a function that at the "index" of "the_list" inserts three times the
 # same value. For example if the_list = [0,1,2,3,4] and index = 3 the function
@@ -33,14 +44,25 @@ def repeat_at_index(the_list, index):
 # write a function that checks whether the word is a palindrome, i.e. it reads
 # the same forward and backwards
 def palindrome_word(word):
-    return
+        reversed_word = word[::-1]
+        if word == reversed_word:
+            print('True')
+        else: 
+            print('False')
 
 # write a function that checks whether the sentence is a palindrome, i.e. it
 # read the same forward and backward. Ignore all spaces and other characters
 # like fullstops, commas, etc. Also do not consider whether the letter is
 # capital or not. 
 def palindrome_sentence(sentence):
-    return
+    reversed_sentence = sentence[::-1]
+    a = reversed_sentence.replace(" ", "")
+    b = sentence.replace(" ", "")
+    if b == a:
+        print('True')
+    else: 
+        print('False')
+
 
 # write a function that concatenates two sentences. First the function checks
 # whether the sentence meets the following criteria: it starts with a capital
@@ -49,7 +71,13 @@ def palindrome_sentence(sentence):
 # the end.  The concatenated sentence must have no white space at the beginning
 # or at the end and the must be exactly one space after the end of the first
 # sentence. 
-def concatenate_sentences(sentenece1, sentence2):
+def concatenate_sentences(sentence1, sentence2):
+    if sentence1[0].isupper() and sentence1.endswith(('.', '?', '!')):
+        new_sentence1 = sentence1.lstrip()
+        new_sentence2 = sentence2.lstrip()
+        print(new_sentence1 + ' ' + new_sentence2)
+    else:
+        print('It doesnt work')
     return
 
 
@@ -58,11 +86,19 @@ def concatenate_sentences(sentenece1, sentence2):
 # write a function that checks whether there is a record with given key in the
 # dictionary. Return True or False.
 def index_exists(dictionary, key):
+    if key in dictionary:
+        print('True')
+    else:
+        print('False')
     return
 
 # write a function which checks whether given value is stored in the
 # dictionary. Return True or False.
 def value_exists(dictionary, value):
+    if value in dictionary:
+        print('True')
+    else:
+        print('False')
     return
 
 # write a function that returns a new dictionary which contains all the values
